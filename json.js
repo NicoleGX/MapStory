30d8b599ee44d016f28d5567c94d487b3daedf52
Nicole <553043565@qq.com>
2018-10-09 21:28:17 +0800

    add search tips

--- json.js
+++ json.js
@@ -1,5 +1,33 @@
 var markerArr = [
   {
+    lng: 113.262626,
+    lat: 23.123541,
+    icon: 'icon/6 32.png',
+    title: '如家快捷酒店(广州解放中路公园前地铁站店)',
+    content: ''
+  },
+  {
+    lng: 113.26817,
+    lat: 23.121252,
+    icon: 'icon/6 32.png',
+    title: '牛小灶牛腩牛杂煲',
+    content: ''
+  },
+  {
+    lng: 113.324452,
+    lat: 23.113248,
+    icon: 'icon/6 32.png',
+    title: '广州塔',
+    content: ''
+  },
+  {
+    lng: 113.543119,
+    lat: 22.191816,
+    icon: 'icon/6 32.png',
+    title: '义顺牛奶公司',
+    content: ''
+  },
+  {
     lng: 113.574707,
     lat: 22.116542,
     icon: 'icon/6 32.png',
@@ -25,6 +53,62 @@ var markerArr = [
     lat: 22.114033,
     icon: 'icon/6 32.png',
     title: '路环辉记美食茶座',
+    content: ''
+  },
+  {
+    lng: 113.562228,
+    lat: 22.150505,
+    icon: 'icon/6 32.png',
+    title: '莫义记',
+    content: ''
+  },
+  {
+    lng: 113.565176,
+    lat: 22.15117,
+    icon: 'icon/6 32.png',
+    title: '龙环葡韵住宅式博物馆',
+    content: ''
+  },
+  {
+    lng: 113.566429,
+    lat: 22.152358,
+    icon: 'icon/6 32.png',
+    title: '大潭山瞭望台',
+    content: ''
+  },
+  {
+    lng: 113.566624,
+    lat: 22.145307,
+    icon: 'icon/6 32.png',
+    title: '威尼斯人',
+    content: ''
+  },
+  {
+    lng: 113.566608,
+    lat: 22.140581,
+    icon: 'icon/6 32.png',
+    title: '巴黎人',
+    content: ''
+  },
+  {
+    lng: 113.543045,
+    lat: 22.194955,
+    icon: 'icon/6 32.png',
+    title: '南屏雅叙',
+    content: ''
+  },
+  {
+    lng: 113.542175,
+    lat: 22.190801,
+    icon: 'icon/6 32.png',
+    title: '最香饼家',
+    content: ''
+  },
+  {
+    lng: 113.547654,
+    lat: 22.192859,
+    icon: 'icon/6 32.png',
+    title: '番茄屋美食',
     content: ''
   }
 ]
