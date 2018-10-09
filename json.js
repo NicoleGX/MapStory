d4f01e9249fc9c1186320774f016106934d4c4ab
Nicole <553043565@qq.com>
2018-10-09 11:10:32 +0800

    first upload

---
+++ json.js
@@ -0,0 +1,30 @@
+var markerArr = [
+  {
+    lng: 113.574707,
+    lat: 22.116542,
+    icon: 'icon/6 32.png',
+    title: '黑沙海滩',
+    content: ''
+  },
+  {
+    lng: 113.556489,
+    lat: 22.115325,
+    icon: 'icon/6 32.png',
+    title: '安德鲁饼店',
+    content: ''
+  },
+  {
+    lng: 113.556581,
+    lat: 22.113958,
+    icon: 'icon/6 32.png',
+    title: '路环圣方济各圣堂',
+    content: ''
+  },
+  {
+    lng: 113.557428,
+    lat: 22.114033,
+    icon: 'icon/6 32.png',
+    title: '路环辉记美食茶座',
+    content: ''
+  }
+]
